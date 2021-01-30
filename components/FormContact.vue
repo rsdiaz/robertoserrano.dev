87139a7b20dfc6a897a1a549ad233f0e451ab687
rsdiaz <robertoserrano83@gmail.com>
2021-01-30 22:17:09 +0100

    Contact page

--- components/FormContact.vue
+++ components/FormContact.vue
@@ -1,69 +1,69 @@
 <template>
-  <div class="card card-content">
-    <form action="https://formspree.io/moqewooe" method="POST">
-      <div class="field">
-        <label class="label">Nombre</label>
-        <div class="control">
-          <input
-            id="fname"
-            v-model="form.nombre"
-            class="input"
-            :class="{ 'is-success': form.nombre }"
-            type="text"
-            placeholder="Aquí, tu nombre"
-            required
-          />
-        </div>
-        <p v-if="form.nombre" class="help is-success">
-          Nombre introducido correctamente
-        </p>
-        <p v-else class="help is-danger">
-          Se necesita un nombre para enviar el formulario
-        </p>
-      </div>
-      <div class="field">
-        <label class="label">Email</label>
-        <div class="control">
-          <input
-            v-model="form.email"
-            class="input"
-            type="email"
-            placeholder="Aquí, tu email"
-            name="_replyto"
-            required
-          />
-        </div>
-        <p v-if="validEmail(form.email)" class="help is-success">
-          El email es valido...
-        </p>
-        <p v-else class="help is-danger">
-          Se necesita un email para enviar el formulario
-        </p>
-      </div>
-      <div class="field">
-        <label class="label">Mensaje</label>
-        <div class="control">
-          <textarea
-            v-model="form.mensaje"
-            class="textarea"
-            placeholder="Mensaje"
-            name="message"
-            required
-          >
-          </textarea>
-          <p v-if="!form.mensaje" class="help is-danger">
-            Necesito un mesaje para responderte.
-          </p>
-        </div>
-      </div>
+  <form method="POST" novalidate="true" @submit="checkForm">
+    <div class="field">
+      <label class="label">Nombre</label>
       <div class="control">
-        <button class="button is-link" :disabled="false">Enviar</button>
+        <input
+          id="fname"
+          v-model="form.nombre"
+          class="input"
+          :class="{ 'is-success': form.nombre }"
+          type="text"
+          placeholder="Aquí, tu nombre"
+          required
+        />
       </div>
-    </form>
-  </div>
+      <p
+        v-if="form.errors.nameError || form.nombre == null"
+        class="help is-error"
+      >
+        Se necesita un nombre para enviar el formulario
+      </p>
+    </div>
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <input
+          v-model="form.email"
+          class="input"
+          type="email"
+          placeholder="Aquí, tu email"
+          name="_replyto"
+          required
+        />
+      </div>
+      <p v-if="form.errors.emailError && form.email == 0" class="help is-error">
+        Se necesita un email valido para que te pueda responder.
+      </p>
+    </div>
+    <div class="field">
+      <label class="label">Mensaje</label>
+      <div class="control">
+        <textarea
+          v-model="form.mensaje"
+          class="textarea"
+          placeholder="Mensaje"
+          name="message"
+          required
+        >
+        </textarea>
+        <p
+          v-if="form.errors.msgError && form.mensaje == 0"
+          class="help is-error"
+        >
+          Necesito un mensaje para responderte.
+        </p>
+      </div>
+    </div>
+    <div class="control">
+      <input type="submit" class="button is-link" value="Enviar" />
+    </div>
+  </form>
 </template>
 
 <script>
+import { required, minLength, email } from 'vuelidate/lib/validators'
+
 export default {
   data() {
     return {
@@ -71,14 +71,29 @@ export default {
         nombre: null,
         email: null,
         mensaje: null,
-        valid: false,
       },
     }
   },
   methods: {
-    /* checkForm() {
-      console.log(this.form.nombre)
-    }, */
+    checkForm(e) {
+      if (!this.form.nombre) {
+        this.form.errors.nameError = true
+      } else {
+        this.form.errors.nameError = false
+      }
+
+      if (!this.form.email) {
+        this.form.errors.emailError = true
+      } else if (!this.validEmail(this.form.email)) {
+        this.form.errors.emailError = true
+      }
+
+      if (!this.form.mensaje) {
+        this.form.errors.msgError = true
+      }
+
+      e.preventDefault()
+    },
     validEmail: (email) => {
       // eslint-disable-next-line no-useless-escape
       const regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
--- pages/contacto.vue
+++ pages/contacto.vue
@@ -4,7 +4,29 @@
     <TopAngle />
     <ResponsiveNav />
     <Header :title="title" :subtitle="subtitle" />
-    <h1>Pagina contacto</h1>
+    <section class="container pb-6">
+      <p>
+        Puedes ponerte en contacto conmigo rellenando el siguiente formulario.
+        En el campo mensaje te agradecería que pusieras toda la información
+        posible para que mi respuesta sea lo mas acertada posible.
+      </p>
+      <p>O tal vez quieras escribirme a través de Twitter</p>
+      <p>
+        Escríbeme si estas interesado en mis servicios, si tienes alguna duda o
+        simplemente si quieres saludarme.
+      </p>
+      <p>Una vez reciba tu mensaje, no tardare mucho en responderte. 👍</p>
+    </section>
+    <section class="container form-container">
+      <div class="columns">
+        <div class="column is-6">
+          <h2 class="subtitle is-3 has-text-centered">
+            Formulario de contacto
+          </h2>
+          <FormContact />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -18,3 +40,35 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.form-container {
+  div.columns {
+    justify-content: center;
+  }
+  input[type='text'],
+  input[type='email'] {
+    width: 100%;
+    border-radius: 0;
+    border: none;
+    padding: 12px;
+    margin-bottom: 18px;
+  }
+  textarea {
+    width: 100%;
+    padding: 12px;
+    margin-bottom: 18px;
+  }
+  .is-error {
+    padding: 12px;
+    background: #202020;
+    color: var(--link-color);
+  }
+  .label {
+    display: block;
+    margin: 5px 0;
+    font-weight: 900;
+    color: var(--link-color);
+  }
+}
+</style>
